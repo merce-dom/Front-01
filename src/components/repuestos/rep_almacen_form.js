56aaee78b155b9732e0279e58c5888652543f26c
merce-dom <mdomenejuan@gmail.com>
2021-09-20 13:38:03 +0200

    Botón Nuevo y Actualizar Almacén

--- src/components/repuestos/rep_almacen_form.js
+++ src/components/repuestos/rep_almacen_form.js
@@ -52,7 +52,7 @@ const RepAlmacenForm = ({nombre, empresa, almacen_id}) => {
         })
         .then( res => { 
             console.log(res);
-            // window.location.href = '/estructura/zonas';
+            window.location.href = "/repuestos/almacenes/";
         })
         .catch(err => { console.log(err);})
         
@@ -71,7 +71,7 @@ const RepAlmacenForm = ({nombre, empresa, almacen_id}) => {
         })
         .then( res => { 
             console.log(res);
-            // window.location.href = '/estructura/zonas';
+            window.location.href = "/repuestos/almacenes/";
         })
         .catch(err => { console.log(err);})
         
@@ -124,14 +124,11 @@ const RepAlmacenForm = ({nombre, empresa, almacen_id}) => {
                     </Form>
                 </Col>
             </Row>
-            <Form.Row className="justify-content-center">
-                <Link to='/repuestos/almacenes'>
-                    {almacen_id ?
-                        <Button variant="info" type="submit" className={'mr-1'} onClick={actualizarDatos, linkVertical}>Actualizar</Button> :
-                        <Button variant="info" type="submit" className={'mr-1'} onClick={nuevoDatos}>Guardar</Button>
-                    }
-                </Link>
-                
+            <Form.Row className="justify-content-center">                
+                {almacen_id ?
+                    <Button variant="info" type="submit" className={'mr-1'} onClick={actualizarDatos}>Actualizar</Button> :
+                    <Button variant="info" type="submit" className={'mr-1'} onClick={nuevoDatos}>Guardar</Button>
+                }                       
                 <Link to='/repuestos/almacenes'>
                     <Button variant="warning" className={'ml-1'} >
                         Cancelar
